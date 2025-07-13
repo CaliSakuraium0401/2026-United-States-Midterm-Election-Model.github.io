f3af07e3ddaa510624ca5f099b63b39ff288884d
CaliSakuraium0401 <98989369+CaliSakuraium0401@users.noreply.github.com>
2025-07-13 15:27:12 -0500

    rewrote code to include all parties and to account for off cycle election years

--- senate_results.py
+++ senate_results.py
@@ -1,71 +1,151 @@
 import pandas as pd
 import numpy as np
+print("✓ | #0 Import Libraries")
 
 # Read the data from the github into a dataframe.
 senate_election_data = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/election-results/refs/heads/main/election_results_senate.csv')
+print("✓ | #1 Read Data")
 
-min_year = 2024
+party_map = {
+    "DEM": "Democratic",
+    "REP": "Republican",
+    "LIB": "Libertarian",
+    "W": "Write-In",
+    "IND": "Independent",
+    "OTH": "Other",
+    "GRE": "Green",
+    "SWP": "Socialist Workers",
+    "CON": "Constitution",
+    "NLP": "Natural Law",
+    "NPA": "No Party Affiliation",
+    "REF": "Reform",
+    "AMP": "American Party",
+    "CRV": "Civic Reform",
+    "UN": "Unity",
+    "IAP": "Independent American",
+    "WFP": "Working Families",
+    "IDP": "Independence",
+    "UST": "U.S. Taxpayers",
+    "AIP": "American Independent",
+    "LBU": "Liberty Union",
+    "PAF": "Peace and Freedom",
+    "GRT": "Grassroots",
+    "COM": "Communist",
+    "NAP": "New Alliance",
+    "LBL": "Liberty Bell",
+    "RTL": "Right to Life",
+    "PG": "Progressive",
+    "CIT": "Citizen's",
+    "SUS": "Sustaining",
+    "SLP": "Socialist Labor",
+    "CNC": "Concerned Citizens",
+    "LBR": "Labor",
+    "IDE": "Independent Democrat",
+    "AKI": "Alaskan Independence",
+    "UTY": "Unity",
+    "N": "Nonpartisan",
+    "LAB": "Labor",
+    "ACP": "American Constitution",
+    "LMN": "Lemon",
+    "PRO": "Prohibition",
+    "MTP": "Mountain",
+    "AVP": "American Veterans",
+    "IGR": "Independent Green",
+    "NNE": "None",
+    "SEP": "Socialist Equality",
+    "SOC": "Socialist",
+    "PCH": "Peace",
+    "DRP": "Democratic-Republican",
+    "CPC": "Conservative",
+    "LRU": "La Raza Unida",
+    "CFP": "Constitutional Freedom",
+    "DGR": "Democratic Green",
+    "FTP": "Freedom",
+    "HRP": "Human Rights",
+    "MJP": "Majority Party",
+    "MRP": "Moderate Republican",
+    "LMP": "Liberty Movement",
+    "COU": "Country",
+    "GMP": "Green Mountain",
+    "GLC": "Green Liberty",
+    "NEB": "Nebraska",
+    "NOP": "No Party",
+    "RES": "Rescue",
+    "NDP": "National Democratic",
+    "NON": "Nonpartisan",
+    "TEA": "Tea Party",
+    "TLP": "Taxpayers",
+    "UC": "United Citizens",
+    "VET": "Veterans",
+    "WTP": "We The People"
+}
+print("✓ | #2 Create Party Map")
+
+senate_election_data['party_name'] = senate_election_data['ballot_party'].replace(party_map)
+print("✓ | #3 Apply Party Map")
+
+min_year = 1998
 max_year = 2026
 year = min_year
+step = 4
 
 while year != 2026:
     senate_election_year = senate_election_data[senate_election_data['cycle'] == year]
-    senate_election_year = senate_election_year[(senate_election_year['ballot_party'] == "DEM") | (senate_election_year['ballot_party'] == "REP")]
-    senate_election_year = senate_election_year.pivot_table(
-            index=['state_abbrev', 'state', 'office_seat_name', 'special'],
-            columns='ballot_party',
-            values=['candidate_name', 'votes', 'percent'],
-            aggfunc='first'
+    
+    senate_election_year = senate_election_year.drop(
+            [
+                'id',
+                'race_id',
+                'office_id',
+                'office_name',
+                'cycle',
+                'stage',
+                'party',
+                'candidate_id',
+                'politician_id',
+                'unopposed',
+                'alt_result_text',
+                'source'
+            ],
+            axis=1
         )
 
-    senate_election_year.columns = ['_'.join(col).strip() for col in senate_election_year.columns.values]
-    senate_election_year = senate_election_year.reset_index()
-
-    # Rename the columns for user friendlyness.
     senate_election_year.rename(
         columns={
             'state_abbrev': 'state_code',
             'state': 'state',
-            'seat_class': 'Office Seat Name',
-            'special': 'special',
-            'candidate_name_DEM': 'dem_candidate',
-            'candidate_name_REP': 'rep_candidate',
-            'percent_DEM': 'dem_per',
-            'percent_REP': 'rep_per',
-            'votes_DEM': 'dem_votes',
-            'votes_REP': 'rep_votes'
+            'office_seat_name': 'seat_class',
+            'ballot_party': 'party_code'
         },
         inplace=True
     )
 
-    senate_election_year['dem_candidate'] = senate_election_year['dem_candidate'].fillna("None")
-    senate_election_year['rep_candidate'] = senate_election_year['rep_candidate'].fillna("None")
-    senate_election_year['dem_per'] = senate_election_year['dem_per'].fillna(0)
-    senate_election_year['rep_per'] = senate_election_year['rep_per'].fillna(0)
-    senate_election_year['dem_votes'] = senate_election_year['dem_votes'].fillna(0)
-    senate_election_year['rep_votes'] = senate_election_year['rep_votes'].fillna(0)
-
-    # Create array that will be the new order of the data frame.
-    order = [
-        'state_code',
-        'state',
-        'office_seat_name',
-        'special',
-        'dem_candidate',
-        'rep_candidate',
-        'dem_votes',
-        'rep_votes',
-        'dem_per',
-        'rep_per'
-    ]
+    senate_election_year['candidate_name'] = senate_election_year['candidate_name'].fillna("None")
+    senate_election_year['party_code'] = senate_election_year['party_code'].fillna("W")
+    senate_election_year['party_name'] = senate_election_year['party_name'].fillna("Write-In")
+    senate_election_year['votes'] = senate_election_year['votes'].fillna(0)
+    senate_election_year['percent'] = senate_election_year['percent'].fillna(0)
+    senate_election_year['ranked_choice_round'] = senate_election_year['ranked_choice_round'].fillna('None')
+    senate_election_year['winner'] = senate_election_year['winner'].fillna('False')
+    
+    senate_election_year = senate_election_year.groupby(['state_code', 'state', 'seat_class', 'special', 'candidate_name',
+       'party_code', 'party_name', 'ranked_choice_round', 'winner'], as_index=False).sum()
+    
+    order = ['state_code', 'state', 'seat_class', 'special', 'candidate_name',
+       'party_code', 'party_name', 'ranked_choice_round', 'votes', 'percent', 'winner']
+    
     senate_election_year = senate_election_year[order]
-    # Creates 'winner' column basd off whoever has more votes.
-    senate_election_year['winner'] = np.where(
-        senate_election_year['dem_votes'] > senate_election_year['rep_votes'],
-        'Democrat',
-        'Republican'
+        
+    senate_election_year = senate_election_year.sort_values(
+        by=['state_code', 'percent'],
+        ascending=[True, False]
     )
 
-    senate_election_year.to_csv(f'election_data/senate/senate_election_{year}.csv', index=False)
-    print(f'Completed: senate_election_{year}.csv' )
-    year += 2
+    filename = f'senate_election_{year}'
+    
+    if len(senate_election_year) >= 1:
+        senate_election_year.to_csv(f'election_data/senate/{filename}.csv', index=False)
+        print(f"✓ | #{step} Completed {filename}")
+    
+    year += 1
+    step += 1
